832d1d9aef73c3f489483d602aa79e0daf4b806d
ayoub-lwela <a.lwela@uot.edu.ly>
2021-07-25 09:43:25 +0200

    Add front end.

---
+++ MMSystem/client_app/src/services/DataService.js
@@ -0,0 +1,11 @@
+//import axios from 'axios';
+import securityService from './SecurityService.js';
+import DashboardService from './DashboardService.js';
+
+export default {
+
+    // ...securityService,
+    securityService: securityService,
+    DashboardService: DashboardService,
+
+}
