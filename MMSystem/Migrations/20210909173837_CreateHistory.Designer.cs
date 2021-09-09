98c20f17cbd622f01bc4b52407a031d3b013e372
AbuBaker Eljobile <abubakereljobile@gmail.com>
2021-09-09 21:44:27 +0300

    DashBord Number & History

--- MMSystem/Migrations/20210909173837_CreateHistory.Designer.cs
+++ MMSystem/Migrations/20210909173837_CreateHistory.Designer.cs
@@ -4,14 +4,16 @@ using MMSystem.Model;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace MMSystem.Migrations
 {
     [DbContext(typeof(AppDbCon))]
-    partial class AppDbConModelSnapshot : ModelSnapshot
+    [Migration("20210909173837_CreateHistory")]
+    partial class CreateHistory
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ MMSystem/Model/ViewModel/Dashbord.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MMSystem.Model.ViewModel
+{
+    public class Dashbord
+    {
+        public int TotaleSender { get; set; }
+        public int TotaleReceived { get; set; }
+        public int TotaleExpord { get; set; }
+    }
+}
