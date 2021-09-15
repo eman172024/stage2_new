bbc537e82b7ec8f4f630591185de1749d901ff36
Halim <Halim@DESKTOP-V98H6QJ>
2021-09-15 13:28:50 +0300

    fix bug in dto

--- MMSystem/Model/Dto/MailDto.cs
+++ MMSystem/Model/Dto/MailDto.cs
@@ -13,7 +13,7 @@ namespace MMSystem.Model.Dto
         public string Date_Of_Mail { get; set; }
         public string Mail_Summary { get; set; }
         public string clasification { get; set; }
-        public string Mail_Type { get; set; }
+        public int Mail_Type { get; set; }
         public int Genaral_inbox_Number { get; set; }
         public int Genaral_inbox_year { get; set; }
         public string Action_Required { get; set; }
