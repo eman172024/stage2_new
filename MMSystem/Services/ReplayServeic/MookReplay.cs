efb7cb9714f3fe2bfdec632007789700b98f54cc
Husam Daza <82757526+husamyousefdaza@users.noreply.github.com>
2022-06-01 12:51:10 +0200

    Add scanner method to Replay form

--- MMSystem/Services/ReplayServeic/MookReplay.cs
+++ MMSystem/Services/ReplayServeic/MookReplay.cs
@@ -19,7 +19,7 @@ namespace MMSystem.Services.ReplayServeic
     {
         private IWebHostEnvironment iwebHostEnvironment;
 
-       public int flag;
+        public int flag;
         public MookReplay(AppDbCon data, IMapper mapper, IWebHostEnvironment environment)
         {
             _data = data;
@@ -30,8 +30,66 @@ namespace MMSystem.Services.ReplayServeic
 
         private readonly AppDbCon _data;
         private readonly IMapper _mapper;
-      
 
+        public async Task<MVM> GetAllReplay(int depid, int mailId)
+        {
+            //  try
+            //   {
+
+            //    Send_to send_ = await _data.Sends.FirstOrDefaultAsync(x => x.MailID == mailId &&x.to==depid);
+            //    MVM mVM = new MVM();
+            //    mVM.list = await _data.Replies.OrderBy(x=>x.ReplyId).Where(x=>x.send_ToId==send_.Id).ToListAsync();
+            //    List<ReplayDto> replays = _mapper.Map<List<Reply>, List<ReplayDto>>(list);
+            //    return replays;
+            //}
+            //catch (Exception)
+            //{
+
+            //    throw;
+            //}
+            try
+            {
+
+                MVM model = new MVM();
+
+
+                Send_to c = await _data.Sends.Where(x => x.to == depid && x.MailID == mailId && x.State == true ).FirstOrDefaultAsync();
+
+                model.list = await (from x in _data.Replies.Where(x => x.send_ToId == c.Id )
+                                        //       join y in _dbCon.Reply_Resources on x.ReplyId equals y.ReplyId
+                                    select new RViewModel
+                                    {
+                                        reply = _mapper.Map<Reply, ReplayDto>(x),
+                                        Resources = _mapper.Map<List<Reply_Resources>, List<Reply_ResourcesDto>>(x._Resources)
+                                    }).ToListAsync();
+
+
+                foreach (var item in model.list)
+                {
+                    foreach (var item2 in item.Resources)
+                    {
+                        string x = item2.path;
+                        if (File.Exists(x))
+                        {
+                            item2.path = await tobase64(x);
+
+                        }
+
+                    }
+                }
+
+
+                return model;
+
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
         public async Task<bool> Add(Reply model)
         {
             try
@@ -55,7 +113,26 @@ namespace MMSystem.Services.ReplayServeic
                 throw;
             }
         }
+        public async Task<bool> Update(Reply model)
+        {
 
+            Reply reply = await _data.Replies.FindAsync(model.ReplyId);
+
+            if (reply != null)
+            {
+
+                reply.mail_detail = model.mail_detail;
+                reply.state = model.state;
+                reply.state = true;
+                reply.IsSend = true;
+
+                _data.Replies.Update(reply);
+                await _data.SaveChangesAsync();
+
+                return true;
+            }
+            return false;
+        }
         public async Task<bool> Delete(int id)
         {
             try
@@ -119,118 +196,160 @@ namespace MMSystem.Services.ReplayServeic
             }
         }
 
- 
+        public async Task<bool> UpdateReplay(ReplyViewModel model)
+        {
+
+
+            bool result;
+            if (model.reply.ReplyId != 0)
+            {
+                model.reply.send_ToId = model.send_ToId;
+                model.reply.Date = DateTime.Now;
+                model.reply.state = true;
+                model.reply.IsSend = true;
+                model.reply.mail_detail = model.reply.mail_detail;
+
+                result = await Update(model.reply);
+                if (result)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                result = await Add(model.reply);
+                if (result)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+        }
+
+
         public async Task<bool> AddReplay(ReplyViewModel model)
         {
             Send_to send = await _data.Sends.FindAsync(model.send_ToId);
-           
+
             switch (model.from)
             {
                 case 1:
-                  flag = 5;
+                    flag = 5;
                     break;
                 case 2:
                     flag = 4;
                     break;
-                default:break;
+                default: break;
             }
 
 
-            if (send != null) {
+            if (send != null)
+            {
 
-                send.flag =flag;
-
+                send.flag = flag;
                 model.reply.send_ToId = model.send_ToId;
                 model.reply.Date = DateTime.Now;
                 model.reply.state = true;
+                model.reply.UserId = model.userId;
                 send.time_of_read = model.reply.Date;
-               
+                model.reply.IsSend = true;
+
                 _data.Update(send);
                 await _data.SaveChangesAsync();
 
-                bool result = await Add(model.reply);
-                if (result) {
+                bool result = await UpdateReplay(model);
+
+                if (result)
+                {
 
                     return true;
                 }
                 return false;
-            
+
             }
             return false;
         }
 
-        public async Task<MVM> GetAllReplay( int depid,int mailId)
+
+        public async Task<bool> AddReplayFromDesktop(ReplyViewModel model)
         {
-            //  try
-            //   {
+            Send_to send = await _data.Sends.FindAsync(model.send_ToId);
 
-            //    Send_to send_ = await _data.Sends.FirstOrDefaultAsync(x => x.MailID == mailId &&x.to==depid);
-            //    MVM mVM = new MVM();
-            //    mVM.list = await _data.Replies.OrderBy(x=>x.ReplyId).Where(x=>x.send_ToId==send_.Id).ToListAsync();
-            //    List<ReplayDto> replays = _mapper.Map<List<Reply>, List<ReplayDto>>(list);
-            //    return replays;
-            //}
-            //catch (Exception)
-            //{
+            switch (model.from)
+            {
+                case 1:
+                    flag = 5;
+                    break;
+                case 2:
+                    flag = 4;
+                    break;
+                default: break;
+            }
 
-            //    throw;
-            //}
-            try
+
+            if (send != null)
             {
 
-                MVM model = new MVM();
+                // send.flag = flag;
 
-               
-                Send_to c = await _data.Sends.Where(x => x.to == depid && x.MailID == mailId && x.State == true).FirstOrDefaultAsync();
-            
-                model.list = await (from x in _data.Replies.Where(x => x.send_ToId == c.Id)
-                                        //       join y in _dbCon.Reply_Resources on x.ReplyId equals y.ReplyId
-                                    select new RViewModel
-                                    {
-                                        reply = _mapper.Map<Reply, ReplayDto>(x),
-                                        Resources = _mapper.Map<List<Reply_Resources>, List<Reply_ResourcesDto>>(x._Resources)
-                                    }).ToListAsync();
+                model.reply.send_ToId = model.send_ToId;
+                model.reply.Date = DateTime.Now;
+                model.reply.state = true;
+                model.reply.IsSend = false;
+                // send.time_of_read  = model.reply.Date;
 
-              
-                foreach (var item in model.list)
+                //  _data.Update(send);
+                // await _data.SaveChangesAsync();
+
+                bool result = await Add(model.reply);
+
+                if (result)
                 {
-                    foreach (var item2 in item.Resources)
-                    {
-                        string x = item2.path;
-                        if (File.Exists(x))
-                        {
-                            item2.path = await tobase64(x);
 
-                        }
-
-                    }
+                    return true;
                 }
-
-
-                return model;
-
-
+                return false;
             }
-            catch (Exception)
-            {
-
-                throw;
-            }
-
+            return false;
         }
 
-
-      
-        public Task<bool> Update(Reply model)
+        public async Task<bool> UpdateResources(Reply model)
         {
-            throw new NotImplementedException();
-        }
 
+            Reply reply = await _data.Replies.FindAsync(model.ReplyId);
+
+            if (reply != null)
+            {
+                List<Reply_Resources> resources = await _data.Reply_Resources.Where(x => x.ReplyId == model.ReplyId && x.State == true).ToListAsync();
+
+                if (resources != null)
+                {
+                    foreach (var item in resources)
+                    {
+                        item.IsSend = true;
+                        _data.Reply_Resources.Update(item);
+                    }
+                    await _data.SaveChangesAsync();
+
+                    return true;
+                }
+                return false;
+            }
+            return false;
+        }
         public async Task<bool> AddResources(Reply_Resources resources)
         {
             try
             {
-                if(resources!=null)
+                if (resources != null)
 
                 {
                     await _data.Reply_Resources.AddAsync(resources);
@@ -239,8 +358,6 @@ namespace MMSystem.Services.ReplayServeic
 
                 }
 
-
-
                 return false;
             }
             catch (Exception)
@@ -250,18 +367,15 @@ namespace MMSystem.Services.ReplayServeic
             }
         }
 
-       
+
 
         public async Task<bool> Uplode(Uplode file)
-      {
+        {
             try
-
-
             {
-                
 
                 bool result = false;
-              
+
                 string year = DateTime.Now.Year.ToString();
                 string Month = DateTime.Now.Month.ToString();
                 string day = DateTime.Now.Day.ToString();
@@ -292,11 +406,11 @@ namespace MMSystem.Services.ReplayServeic
                 if (!Directory.Exists(last))
                     Directory.CreateDirectory(last);
 
-             
+
 
                 else { }
 
-               
+
 
                 if (file.list.Count > 0)
                 {
@@ -320,10 +434,12 @@ namespace MMSystem.Services.ReplayServeic
                         reply.path = path + extention;
                         reply.order = item.index;
                         reply.State = true;
+                        reply.IsSend = true;
+
+
                         bool res = await AddResources(reply);
                         if (res)
                         {
-
                             result = true;
                         }
                         else
@@ -346,23 +462,19 @@ namespace MMSystem.Services.ReplayServeic
                 throw;
             }
 
-
-
-
-
         }
 
         public async Task<List<RViewModel>> GetResourse(int id)
         {
             try
             {
-                List<RViewModel> list = await(from x in _data.Replies.Where(x => x.send_ToId == id)
-                                              join y in _data.Reply_Resources on x.ReplyId equals y.ReplyId
-                                              select new RViewModel
-                                              {
-                                                  reply = _mapper.Map<Reply, ReplayDto>(x),
-                                                  Resources = _mapper.Map<List<Reply_Resources>, List<Reply_ResourcesDto>>(_data.Reply_Resources.Where(x => x.ReplyId == x.ID).ToList())
-                                              }).ToListAsync();
+                List<RViewModel> list = await (from x in _data.Replies.Where(x => x.send_ToId == id)
+                                               join y in _data.Reply_Resources on x.ReplyId equals y.ReplyId
+                                               select new RViewModel
+                                               {
+                                                   reply = _mapper.Map<Reply, ReplayDto>(x),
+                                                   Resources = _mapper.Map<List<Reply_Resources>, List<Reply_ResourcesDto>>(_data.Reply_Resources.Where(x => x.ReplyId == x.ID).ToList())
+                                               }).ToListAsync();
 
                 foreach (var item in list)
                 {
@@ -406,23 +518,22 @@ namespace MMSystem.Services.ReplayServeic
 
         public async Task<int> AddReplayWithPhoto(ReplayPhotoVM replay)
         {
-
             try
             {
-              
+                Reply reply1 = await _data.Replies.FirstOrDefaultAsync(x => x.send_ToId.Equals(replay.send_ToId) && x.IsSend == false && x.state == true && x.UserId == replay.userId);
 
                 ReplyViewModel reply = new ReplyViewModel();
-                reply.send_ToId = replay.reply.send_ToId;
+                reply.send_ToId = replay.send_ToId;
                 reply.from = replay.from;
+                reply.userId = replay.userId;
+                if (reply1 != null) { reply.reply = reply1; reply.reply.mail_detail = replay.reply.mail_detail; } else { reply.reply = replay.reply; }
+                //   
 
-               
-
-                reply.reply = replay.reply;
-
+                //   
                 bool result = await AddReplay(reply);
 
-                if (result) {
-
+                if (result)
+                {
 
                     Historyes historyes = new Historyes();
                     historyes.mailid = replay.mailId;
@@ -433,31 +544,43 @@ namespace MMSystem.Services.ReplayServeic
                     await _data.History.AddAsync(historyes);
                     await _data.SaveChangesAsync();
 
+                    if (reply1 != null)
+                    {
+                        // replay.file.mail_id = reply1.ReplyId;
+                        bool res1 = await UpdateResources(reply.reply);
 
-                    replay.file.mail_id = reply.reply.ReplyId;
-                    if (replay.file.list.Count > 0) {
-
-                        bool res = await Uplode(replay.file);
-                        if (res)
+                        if (res1)
                         {
 
-
-
-                           // historyes.changes = $" اضافة صور للردود   {replay.file.list.Count().ToString()}";
-
-                            //await _data.History.AddAsync(historyes);
-                            //await _data.SaveChangesAsync();
                             return 1;
 
-                    }
-                       
+                        }
+
                         return 2;
+                    }
+                    else
+                    {
+                        replay.file.mail_id = reply.reply.ReplyId;
 
+                        if (replay.file.list.Count > 0)
+                        {
+
+                            bool res = await Uplode(replay.file);
+                            if (res)
+                            {
+                                // historyes.changes = $" اضافة صور للردود   {replay.file.list.Count().ToString()}";
+
+                                //await _data.History.AddAsync(historyes);
+                                //await _data.SaveChangesAsync();
+                                return 1;
+
+                            }
+
+                            return 2;
+
+                        }
                     }
 
-
-
-                
                 }
                 return 3;
 
@@ -469,5 +592,83 @@ namespace MMSystem.Services.ReplayServeic
             }
 
         }
+        public async Task<int> AddReplayWithPhotoFromDeskApp(ReplayPhotoVM replay)
+        {
+            try
+            {
+
+                ReplyViewModel reply = new ReplyViewModel();
+                reply.send_ToId = replay.send_ToId;
+                reply.from = replay.from;
+
+                reply.reply = replay.reply;
+
+                bool result = await AddReplayFromDesktop(reply);
+
+                if (result)
+                {
+                    replay.file.mail_id = reply.reply.ReplyId;
+                    if (replay.file.list.Count > 0)
+                    {
+
+                        bool res = await Uplode(replay.file);
+                        if (res)
+                        {
+                            return 1;
+                        }
+                        return 2;
+                    }
+                }
+                return 3;
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+        public async Task<bool> DeleteNotSendedReply()
+        {
+            try
+            {
+                List<Reply> reply = await _data.Replies.Where(x => x.IsSend.Equals(false) && x.state.Equals(true)).ToListAsync();
+
+                if (reply != null)
+                {
+                    List<Reply_Resources> resourse = await _data.Reply_Resources.Where(x => x.IsSend.Equals(false) && x.State == true).ToListAsync();
+
+
+                    foreach (var item in reply)
+                    {
+                        item.state = false;
+                        _data.Replies.Update(item);
+                    }
+                    await _data.SaveChangesAsync();
+                    if (resourse != null)
+                    {
+                        foreach (var item1 in resourse)
+                        {
+                            item1.State = false;
+                            _data.Reply_Resources.Update(item1);
+                        }
+                    }
+                    await _data.SaveChangesAsync();
+                    return true;
+                }
+
+                return false;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+
+
     }
 }
