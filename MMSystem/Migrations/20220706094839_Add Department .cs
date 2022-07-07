c8f979d4235cba86e88bd1440a27207e982b94f4
Husam Daza <82757526+husamyousefdaza@users.noreply.github.com>
2022-07-07 13:16:03 +0200

    ddd

---
+++ MMSystem/Migrations/20220706094839_Add Department .cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MMSystem.Migrations
+{
+    public partial class AddDepartment : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Departments",
+                keyColumn: "Id",
+                keyValue: 50,
+                column: "DepartmentName",
+                value: "فرع غرب طرابلس");
+
+            migrationBuilder.InsertData(
+                table: "Departments",
+                columns: new[] { "Id", "DepartmentName", "perent", "state" },
+                values: new object[] { 56, "فرع شرق طرابلس", 0, true });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Departments",
+                keyColumn: "Id",
+                keyValue: 56);
+
+            migrationBuilder.UpdateData(
+                table: "Departments",
+                keyColumn: "Id",
+                keyValue: 50,
+                column: "DepartmentName",
+                value: "غرب طرابلس");
+        }
+    }
+}
