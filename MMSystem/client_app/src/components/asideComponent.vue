0348acea126dc2ac159f911da4a5416862a91a06
ayoub-lwela <a.lwela@uot.edu.ly>
2022-10-30 13:32:32 +0200

    guide color.

--- MMSystem/client_app/src/components/asideComponent.vue
+++ MMSystem/client_app/src/components/asideComponent.vue
@@ -137,6 +137,37 @@
                 </svg>
                 <span v-if="toggle_nav"> المستخدمين </span>
               </router-link>
+
+
+
+
+
+              <router-link
+              
+                title=" دليل الالوان "
+                :to="{ name: 'guidecolor' }"
+                class="
+                  text-gray-600
+                  hover:bg-gray-50 hover:text-gray-900
+                  group
+                  rounded-md
+                  py-2
+                  px-2
+                  flex
+                  items-center
+                  justify-center
+                  text-sm
+                  font-medium
+                "
+              >
+                <svg class="text-gray-400 group-hover:text-gray-500 h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M18.364 5.636l-3.536 3.536m0 5.656l3.536 3.536M9.172 9.172L5.636 5.636m3.536 9.192l-3.536 3.536M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-5 0a4 4 0 11-8 0 4 4 0 018 0z" />
+                </svg>
+
+               
+                <span v-if="toggle_nav"> دليل الالوان </span>
+              </router-link>
+
             </div>
 
             <!-- <router-link :to="{ name: 'login' }" class="text-gray-600 hover:bg-gray-50 hover:text-gray-900 group rounded-md py-2 px-2 flex items-center text-sm font-medium">
