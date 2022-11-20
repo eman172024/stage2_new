21a08fa1669d9654fd4c394419513163855e5ef4
ayoub-lwela <a.lwela@uot.edu.ly>
2022-11-20 11:51:19 +0200

    WIP

--- MMSystem/client_app/src/views/mail/sent/sent_form.vue
+++ MMSystem/client_app/src/views/mail/sent/sent_form.vue
@@ -1979,84 +1979,245 @@
                   </div>
 
                   <div v-if="deleteButton" class="flex justify-end ml-6">
-                    <button
-                      v-if="
-                        summary &&
-                          classification &&
-                          (consignees.length != 0 ||
-                            newactionSenders.length != 0) &&
-                          roles.includes('i')
-                      "
-                      @click="deleteMail"
-                      type="button"
-                      id="edit"
-                      class="
-                        w-full
-                        sm:w-auto sm:mr-3
-                        flex
-                        justify-center
-                        items-center
-                        py-2
-                        px-4
-                        border border-transparent
-                        shadow-sm
-                        text-sm
-                        font-medium
-                        rounded-md
-                        border-green-600
-                        text-white
-                        bg-green-600
-                        hover:shadow-lg
-                        focus:shadow-none
-                        duration-300
-                        focus:outline-none
-                      "
-                    >
-                      <!-- onclick="change();" -->
-                      <svg
-                        class="
-                          w-4
-                          h-4
-                          stroke-current
-                          text-white
-                          ml-2
-                          fill-current
+                    <div v-if="roles.includes('3') && mailType == '1'" class="">
+                      <button
+                        v-if="
+                          summary &&
+                            classification &&
+                            (consignees.length != 0 ||
+                              newactionSenders.length != 0)
                         "
-                        version="1.1"
-                        id="Capa_1"
-                        xmlns="http://www.w3.org/2000/svg"
-                        xmlns:xlink="http://www.w3.org/1999/xlink"
-                        x="0px"
-                        y="0px"
-                        viewBox="0 0 477.873 477.873"
-                        style="enable-background: new 0 0 477.873 477.873"
-                        xml:space="preserve"
+                        @click="deleteMail"
+                        type="button"
+                        id="edit"
+                        class="
+                          w-full
+                          sm:w-auto sm:mr-3
+                          flex
+                          justify-center
+                          items-center
+                          py-2
+                          px-4
+                          border border-transparent
+                          shadow-sm
+                          text-sm
+                          font-medium
+                          rounded-md
+                          border-green-600
+                          text-white
+                          bg-green-600
+                          hover:shadow-lg
+                          focus:shadow-none
+                          duration-300
+                          focus:outline-none
+                        "
                       >
-                        <g>
+                        <!-- onclick="change();" -->
+                        <svg
+                          class="
+                            w-4
+                            h-4
+                            stroke-current
+                            text-white
+                            ml-2
+                            fill-current
+                          "
+                          version="1.1"
+                          id="Capa_1"
+                          xmlns="http://www.w3.org/2000/svg"
+                          xmlns:xlink="http://www.w3.org/1999/xlink"
+                          x="0px"
+                          y="0px"
+                          viewBox="0 0 477.873 477.873"
+                          style="enable-background: new 0 0 477.873 477.873"
+                          xml:space="preserve"
+                        >
                           <g>
-                            <path
-                              d="M392.533,238.937c-9.426,0-17.067,7.641-17.067,17.067V426.67c0,9.426-7.641,17.067-17.067,17.067H51.2
-                                        c-9.426,0-17.067-7.641-17.067-17.067V85.337c0-9.426,7.641-17.067,17.067-17.067H256c9.426,0,17.067-7.641,17.067-17.067
-                                        S265.426,34.137,256,34.137H51.2C22.923,34.137,0,57.06,0,85.337V426.67c0,28.277,22.923,51.2,51.2,51.2h307.2
-                                        c28.277,0,51.2-22.923,51.2-51.2V256.003C409.6,246.578,401.959,238.937,392.533,238.937z"
-                            ></path>
+                            <g>
+                              <path
+                                d="M392.533,238.937c-9.426,0-17.067,7.641-17.067,17.067V426.67c0,9.426-7.641,17.067-17.067,17.067H51.2
+                                          c-9.426,0-17.067-7.641-17.067-17.067V85.337c0-9.426,7.641-17.067,17.067-17.067H256c9.426,0,17.067-7.641,17.067-17.067
+                                          S265.426,34.137,256,34.137H51.2C22.923,34.137,0,57.06,0,85.337V426.67c0,28.277,22.923,51.2,51.2,51.2h307.2
+                                          c28.277,0,51.2-22.923,51.2-51.2V256.003C409.6,246.578,401.959,238.937,392.533,238.937z"
+                              ></path>
+                            </g>
                           </g>
-                        </g>
-                        <g>
                           <g>
-                            <path
-                              d="M458.742,19.142c-12.254-12.256-28.875-19.14-46.206-19.138c-17.341-0.05-33.979,6.846-46.199,19.149L141.534,243.937
-                                        c-1.865,1.879-3.272,4.163-4.113,6.673l-34.133,102.4c-2.979,8.943,1.856,18.607,10.799,21.585
-                                        c1.735,0.578,3.552,0.873,5.38,0.875c1.832-0.003,3.653-0.297,5.393-0.87l102.4-34.133c2.515-0.84,4.8-2.254,6.673-4.13
-                                        l224.802-224.802C484.25,86.023,484.253,44.657,458.742,19.142z M434.603,87.419L212.736,309.286l-66.287,22.135l22.067-66.202
-                                        L390.468,43.353c12.202-12.178,31.967-12.158,44.145,0.044c5.817,5.829,9.095,13.72,9.12,21.955
-                                        C443.754,73.631,440.467,81.575,434.603,87.419z"
-                            ></path>
+                            <g>
+                              <path
+                                d="M458.742,19.142c-12.254-12.256-28.875-19.14-46.206-19.138c-17.341-0.05-33.979,6.846-46.199,19.149L141.534,243.937
+                                          c-1.865,1.879-3.272,4.163-4.113,6.673l-34.133,102.4c-2.979,8.943,1.856,18.607,10.799,21.585
+                                          c1.735,0.578,3.552,0.873,5.38,0.875c1.832-0.003,3.653-0.297,5.393-0.87l102.4-34.133c2.515-0.84,4.8-2.254,6.673-4.13
+                                          l224.802-224.802C484.25,86.023,484.253,44.657,458.742,19.142z M434.603,87.419L212.736,309.286l-66.287,22.135l22.067-66.202
+                                          L390.468,43.353c12.202-12.178,31.967-12.158,44.145,0.044c5.817,5.829,9.095,13.72,9.12,21.955
+                                          C443.754,73.631,440.467,81.575,434.603,87.419z"
+                              ></path>
+                            </g>
                           </g>
-                        </g>
-                      </svg>
-                      حذف
-                    </button>
+                        </svg>
+                        حذف
+                      </button>
+                    </div>
+
+                    <div v-if="roles.includes('5') && mailType == '2'" class="">
+                      <button
+                        v-if="
+                          summary &&
+                            classification &&
+                            (consignees.length != 0 ||
+                              newactionSenders.length != 0)
+                        "
+                        @click="deleteMail"
+                        type="button"
+                        id="edit"
+                        class="
+                          w-full
+                          sm:w-auto sm:mr-3
+                          flex
+                          justify-center
+                          items-center
+                          py-2
+                          px-4
+                          border border-transparent
+                          shadow-sm
+                          text-sm
+                          font-medium
+                          rounded-md
+                          border-green-600
+                          text-white
+                          bg-green-600
+                          hover:shadow-lg
+                          focus:shadow-none
+                          duration-300
+                          focus:outline-none
+                        "
+                      >
+                        <!-- onclick="change();" -->
+                        <svg
+                          class="
+                            w-4
+                            h-4
+                            stroke-current
+                            text-white
+                            ml-2
+                            fill-current
+                          "
+                          version="1.1"
+                          id="Capa_1"
+                          xmlns="http://www.w3.org/2000/svg"
+                          xmlns:xlink="http://www.w3.org/1999/xlink"
+                          x="0px"
+                          y="0px"
+                          viewBox="0 0 477.873 477.873"
+                          style="enable-background: new 0 0 477.873 477.873"
+                          xml:space="preserve"
+                        >
+                          <g>
+                            <g>
+                              <path
+                                d="M392.533,238.937c-9.426,0-17.067,7.641-17.067,17.067V426.67c0,9.426-7.641,17.067-17.067,17.067H51.2
+                                          c-9.426,0-17.067-7.641-17.067-17.067V85.337c0-9.426,7.641-17.067,17.067-17.067H256c9.426,0,17.067-7.641,17.067-17.067
+                                          S265.426,34.137,256,34.137H51.2C22.923,34.137,0,57.06,0,85.337V426.67c0,28.277,22.923,51.2,51.2,51.2h307.2
+                                          c28.277,0,51.2-22.923,51.2-51.2V256.003C409.6,246.578,401.959,238.937,392.533,238.937z"
+                              ></path>
+                            </g>
+                          </g>
+                          <g>
+                            <g>
+                              <path
+                                d="M458.742,19.142c-12.254-12.256-28.875-19.14-46.206-19.138c-17.341-0.05-33.979,6.846-46.199,19.149L141.534,243.937
+                                          c-1.865,1.879-3.272,4.163-4.113,6.673l-34.133,102.4c-2.979,8.943,1.856,18.607,10.799,21.585
+                                          c1.735,0.578,3.552,0.873,5.38,0.875c1.832-0.003,3.653-0.297,5.393-0.87l102.4-34.133c2.515-0.84,4.8-2.254,6.673-4.13
+                                          l224.802-224.802C484.25,86.023,484.253,44.657,458.742,19.142z M434.603,87.419L212.736,309.286l-66.287,22.135l22.067-66.202
+                                          L390.468,43.353c12.202-12.178,31.967-12.158,44.145,0.044c5.817,5.829,9.095,13.72,9.12,21.955
+                                          C443.754,73.631,440.467,81.575,434.603,87.419z"
+                              ></path>
+                            </g>
+                          </g>
+                        </svg>
+                        حذف
+                      </button>
+                    </div>
+
+                    <div v-if="roles.includes('4') && mailType == '3'" class="">
+                      <button
+                        v-if="
+                          summary &&
+                            classification &&
+                            (consignees.length != 0 ||
+                              newactionSenders.length != 0)
+                        "
+                        @click="deleteMail"
+                        type="button"
+                        id="edit"
+                        class="
+                          w-full
+                          sm:w-auto sm:mr-3
+                          flex
+                          justify-center
+                          items-center
+                          py-2
+                          px-4
+                          border border-transparent
+                          shadow-sm
+                          text-sm
+                          font-medium
+                          rounded-md
+                          border-green-600
+                          text-white
+                          bg-green-600
+                          hover:shadow-lg
+                          focus:shadow-none
+                          duration-300
+                          focus:outline-none
+                        "
+                      >
+                        <!-- onclick="change();" -->
+                        <svg
+                          class="
+                            w-4
+                            h-4
+                            stroke-current
+                            text-white
+                            ml-2
+                            fill-current
+                          "
+                          version="1.1"
+                          id="Capa_1"
+                          xmlns="http://www.w3.org/2000/svg"
+                          xmlns:xlink="http://www.w3.org/1999/xlink"
+                          x="0px"
+                          y="0px"
+                          viewBox="0 0 477.873 477.873"
+                          style="enable-background: new 0 0 477.873 477.873"
+                          xml:space="preserve"
+                        >
+                          <g>
+                            <g>
+                              <path
+                                d="M392.533,238.937c-9.426,0-17.067,7.641-17.067,17.067V426.67c0,9.426-7.641,17.067-17.067,17.067H51.2
+                                          c-9.426,0-17.067-7.641-17.067-17.067V85.337c0-9.426,7.641-17.067,17.067-17.067H256c9.426,0,17.067-7.641,17.067-17.067
+                                          S265.426,34.137,256,34.137H51.2C22.923,34.137,0,57.06,0,85.337V426.67c0,28.277,22.923,51.2,51.2,51.2h307.2
+                                          c28.277,0,51.2-22.923,51.2-51.2V256.003C409.6,246.578,401.959,238.937,392.533,238.937z"
+                              ></path>
+                            </g>
+                          </g>
+                          <g>
+                            <g>
+                              <path
+                                d="M458.742,19.142c-12.254-12.256-28.875-19.14-46.206-19.138c-17.341-0.05-33.979,6.846-46.199,19.149L141.534,243.937
+                                          c-1.865,1.879-3.272,4.163-4.113,6.673l-34.133,102.4c-2.979,8.943,1.856,18.607,10.799,21.585
+                                          c1.735,0.578,3.552,0.873,5.38,0.875c1.832-0.003,3.653-0.297,5.393-0.87l102.4-34.133c2.515-0.84,4.8-2.254,6.673-4.13
+                                          l224.802-224.802C484.25,86.023,484.253,44.657,458.742,19.142z M434.603,87.419L212.736,309.286l-66.287,22.135l22.067-66.202
+                                          L390.468,43.353c12.202-12.178,31.967-12.158,44.145,0.044c5.817,5.829,9.095,13.72,9.12,21.955
+                                          C443.754,73.631,440.467,81.575,434.603,87.419z"
+                              ></path>
+                            </g>
+                          </g>
+                        </svg>
+                        حذف
+                      </button>
+                    </div>
                   </div>
 
                   <div v-if="saveButton" class="flex justify-end">
