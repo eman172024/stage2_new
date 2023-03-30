98f8eadf7d0907289850686979e4344bf63dfa74
Halim <Halim@DESKTOP-V98H6QJ>
2023-03-30 21:52:43 +0200

    delete updateAt

---
+++ MMSystem/Migrations/20230330195150_deleteUpdateAt.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MMSystem.Migrations
+{
+    public partial class deleteUpdateAt : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "update_at",
+                table: "external_Departments");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "update_at",
+                table: "external_Departments",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
