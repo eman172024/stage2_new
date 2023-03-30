d44bc1988b93f19ac4a836c99c090eeb45a48d7e
Halim <Halim@DESKTOP-V98H6QJ>
2023-03-30 21:46:42 +0200

    fix side_id

---
+++ MMSystem/Migrations/20230330194024_side_.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MMSystem.Migrations
+{
+    public partial class side_ : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_external_Departments_Extrmal_Sections_side_id",
+                table: "external_Departments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_external_Departments_side_id",
+                table: "external_Departments");
+
+            migrationBuilder.DropColumn(
+                name: "side_id",
+                table: "external_Departments");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_external_Departments_side_number",
+                table: "external_Departments",
+                column: "side_number");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_external_Departments_Extrmal_Sections_side_number",
+                table: "external_Departments",
+                column: "side_number",
+                principalTable: "Extrmal_Sections",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_external_Departments_Extrmal_Sections_side_number",
+                table: "external_Departments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_external_Departments_side_number",
+                table: "external_Departments");
+
+            migrationBuilder.AddColumn<int>(
+                name: "side_id",
+                table: "external_Departments",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_external_Departments_side_id",
+                table: "external_Departments",
+                column: "side_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_external_Departments_Extrmal_Sections_side_id",
+                table: "external_Departments",
+                column: "side_id",
+                principalTable: "Extrmal_Sections",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
--- MMSystem/Model/External_Department.cs
+++ MMSystem/Model/External_Department.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,7 +11,7 @@ namespace MMSystem.Model
     {
         [Key]
         public int id { get; set; }
-
+        [ForeignKey("side_")]
         public int side_number { get; set; }
 
       //  public string side_name { get; set; }
