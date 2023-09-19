1e0facbf1306bea6f484f0a97568bfc6392bc7d8
OsamaGR97 <Osamagr97@gmail.com>
2023-09-19 12:46:42 +0200

    adding the resend mock and controller

---
+++ MMSystem/Services/ResendMail/IResendMail.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MMSystem.Services.ResendMail
+{
+    public class IResendMail
+    {
+    }
+}
