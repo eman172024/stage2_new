3b0824b89ba4d2aaa07f93de2ae8faf9bf7d4665
OsamaGR97 <Osamagr97@gmail.com>
2023-11-14 12:14:06 +0200

    add the UpdateResend function

--- MMSystem/Services/ResendMail/MockResendMail.cs
+++ MMSystem/Services/ResendMail/MockResendMail.cs
@@ -28,13 +28,30 @@ namespace MMSystem.Services.ResendMail
         }
 
 
-        private AppDbCon _data { get; }
+        private readonly AppDbCon _data;
         private readonly ISender _sender;
         private readonly IHistory _history;
 
 
 
+        public async Task<bool> Add(Send_to t)
+        {
+            try
+            {
 
+
+                await _data.Sends.AddAsync(t);
+                await _data.SaveChangesAsync();
+                return true;
+
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
         public async Task<bool> SaveResendMail(ResendViewModel mail)
     {
 
@@ -71,7 +88,7 @@ namespace MMSystem.Services.ResendMail
                         sender.resendfrom = mail.actionSenders[i].ResendFrom;
                         sender.update_At = DateTime.Now;
                         sender.State = true;
-                        bool send = await _sender.Add(sender);
+                        bool send = await Add(sender);
                         await _data.SaveChangesAsync();
                         sender = _data.Sends.OrderBy(x => x.Id).LastOrDefault();
 
@@ -84,7 +101,7 @@ namespace MMSystem.Services.ResendMail
                             SNotes.State = true;
 
                          await   _data.section_Notes.AddAsync(SNotes);
-                           await _data.SaveChangesAsync();
+                         await _data.SaveChangesAsync();
 
                         }
 
@@ -108,6 +125,126 @@ namespace MMSystem.Services.ResendMail
             throw;
         }
     }
+
+
+
+        public async Task<bool> UpdateResendMail(ResendViewModel mail)
+        {
+            bool result = false;
+            int j;
+            Section_Notes SNotes = new Section_Notes();
+            int LastID;
+            try
+            {
+                var mail1 = await _data.Mails.FindAsync(mail.Mail_id);
+
+
+                if (mail1 != null)
+
+                {
+                    for (int i = 0; i < mail.actionSenders.Count; i++)
+                    {
+                        Send_to sender = new Send_to(); 
+                        List<Section_Notes> SNote;
+                        List<Send_to> sends;
+                        // virable sends empty when action sender get one more when its add 
+                        sends = _data.Sends.Where(x => x.MailID == mail.Mail_id && x.resendfrom == mail.actionSenders[i].ResendFrom && mail.actionSenders[i].Sendes_to==x.to).ToList();
+
+                       
+                       
+                       
+                        if (sends != null)
+                        {
+                            if (i == (mail.actionSenders.Count - 1))
+                            {
+                                sender.isMulti = true;
+                            }
+                            else
+                            {
+                                sender.isMulti = false;
+                            }
+
+                            if (sends.Count() == 0)
+                               {  j = -1; }else { j = 0; }
+
+                                for ( j=j ; j < sends.Count; j++)
+                            {
+                             
+
+                                if (sends.Count==0)
+                                {
+                                    sender.MailID = mail1.MailID;
+                                    sender.to = mail.actionSenders[i].Sendes_to;
+                                    sender.flag = 1;
+                                    sender.resendfrom = mail.actionSenders[i].ResendFrom;
+                                    sender.update_At = DateTime.Now;
+                                    sender.State = false;
+                                    bool send= await Add(sender);
+                                    await _data.SaveChangesAsync();
+
+                                    sender = _data.Sends.OrderBy(x => x.Id).LastOrDefault();
+
+                                    LastID = sender.Id;
+                                    result = true;
+                                   
+                                    if (mail.actionSenders[i].Note != "")
+                                    {
+                                        SNotes.send_ToId = LastID;
+                                        SNotes.Note = mail.actionSenders[i].Note;
+                                        SNotes.State = true;
+
+                                        await _data.section_Notes.AddAsync(SNotes);
+                                        await _data.SaveChangesAsync();
+                                        result = true;
+
+                                    }
+                                }
+                                else if (mail.actionSenders[i].Sendes_to == sends[j].to && mail.actionSenders[i].ResendFrom == sends[j].resendfrom && mail.Mail_id == sends[j].MailID)
+                                {
+                                    sends[j].MailID = mail1.MailID;
+                                    sends[j].to = mail.actionSenders[i].Sendes_to;
+                                    sends[j].flag = 1;
+                                    sends[j].resendfrom = mail.actionSenders[i].ResendFrom;
+                                    sends[j].update_At = DateTime.Now;
+                                    sends[j].State = false;
+                                     _data.Sends.Update(sends[j]);
+                                    await _data.SaveChangesAsync();
+                                    sender = _data.Sends.OrderBy(x => x.Id).LastOrDefault();
+
+                                    LastID = sender.Id;
+                                    if (j != -1) {
+                                    SNote = _data.section_Notes.Where(x => x.send_ToId == sends[j].Id).ToList();
+                                    if (mail.actionSenders[i].Note != "" )
+                                    {
+                                      //SNote[j].send_ToId = sends[j].Id;
+                                        SNote[j].Note = mail.actionSenders[i].Note;
+                                        SNote[j].State = true;
+
+                                         _data.section_Notes.Update(SNote[j]);
+                                        await _data.SaveChangesAsync();
+                                        result = true;
+
+                                    }
+                                    }
+                                }
+
+                            }
+
+
+                        }
+
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return result;
+
+        }
+
 
         public async Task<bool> deleteSectionsSender(int mail_id, int departmentId, int userid)
         {
