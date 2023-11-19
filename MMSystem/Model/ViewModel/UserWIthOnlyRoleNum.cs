79daba8b88f0961d92a51f41398c8902cc355afc
Husam Daza <82757526+husamyousefdaza@users.noreply.github.com>
2023-11-19 13:31:58 +0200

    admistrator

--- MMSystem/Model/ViewModel/UserWIthOnlyRoleNum.cs
+++ MMSystem/Model/ViewModel/UserWIthOnlyRoleNum.cs
@@ -8,10 +8,14 @@ namespace MMSystem.Model.ViewModel
 {
     public class UserWithOnlyRoleNum
     {
+
         public UserWithOnlyRoleNum()
         {
             this.Listrole = new List<string>();
         }
+        
+        public int parent_id { get; set; }
+        public string parent_name { get; set; }
         public AdministratorDto Administrator { get; set; }
         public List<string> Listrole { get; set; }
 
